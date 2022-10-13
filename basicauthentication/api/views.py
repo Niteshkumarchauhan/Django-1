970ab6a32479fd67a8c37848acce8e0b34afa9b1
Nitesh Kumar Chauhan <110373197+Niteshkumarchauhan@users.noreply.github.com>
2022-10-13 10:03:54 +0530

    Add files via upload

---
+++ basicauthentication/api/views.py
@@ -0,0 +1,14 @@
+from .models import Person
+from . serializers import PersonSerializer
+from rest_framework import viewsets
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
+
+class PersonModelViewSet(viewsets.ModelViewSet):
+  queryset = Person.objects.all()
+  serializer_class = PersonSerializer
+  authentication_classes = [BasicAuthentication]
+  # permission_classes = [IsAuthenticated]
+  # permission_classes = [AllowAny]
+  permission_classes = [IsAdminUser]
+ 
