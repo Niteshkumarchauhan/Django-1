a9b2c299e5094a45658f952643c73e15db11010c
Nitesh Kumar Chauhan <110373197+Niteshkumarchauhan@users.noreply.github.com>
2022-10-13 10:26:00 +0530

    Add files via upload

---
+++ sessionauthentication/sessionauthentication/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path, include
+from api import views
+from rest_framework.routers import DefaultRouter
+
+
+# Creating router object
+router = DefaultRouter()
+
+
+# Register Person viewset with router
+router.register('preinfo', views.PersonModelViewSet, basename='person')
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include( router.urls)),
+    path('auth/',include( 'rest_framework.urls', namespace='rest_framework.urls')),
+]
